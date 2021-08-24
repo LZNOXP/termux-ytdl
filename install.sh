97f39e67f2e0b0ef17c365e746f47826fcf37bf8
LZNOXP <72902682+LZNOXP@users.noreply.github.com>
2021-08-24 09:23:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,5 +17,5 @@ chmod +x ./bin/termux-url-opener
 curl -L https://raw.githubusercontent.com/LZNOXP/termux-ytdl/main/simple-ytdl.py > "simple-ytdl.py"
 
 
-# one liner because this script somehow doesn't reach the read command
-apt-get update && apt-get -y install python ffmpeg && pip install youtube-dl colorama && python3 ~/simple-ytdl.py --setup
+apt-get update && apt-get -y install python ffmpeg && pip install youtube-dl colorama
+python3 ~/simple-ytdl.py --setup
